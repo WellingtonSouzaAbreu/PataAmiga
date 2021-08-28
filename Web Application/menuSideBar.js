ebfc8d073c69380770168b8488c9606bc95722a6
WellingtonSouzaAbreu <wellingtonsouza6300@gmail.com>
2021-08-28 14:12:33 -0400

    Menu options selecionado

---
+++ Web Application/menuSideBar.js
@@ -0,0 +1,46 @@
+let Options = [
+    Animais = [ // Lista
+        'Info',
+        'Imagens',
+        'Adotado, lar temporáio ou disponível',
+        'Cuidados veterinários => Custos veterinários',
+        'Resgate => Colaboradores envolvidos no resgate'
+    ],
+
+    Adocão = [ // Lista
+        'Animal => Info',
+        'Usuário', 
+        'Monitoramento remoto(lista) => Imagens periódicas',
+        'Visitas(lista) => Relatório'
+    ],
+
+    Interessados = [ // Lista
+        'Usuário',
+        'Animal',
+        'Imagens e descrição do quintal'
+    ],
+
+    LarTemporario = [ // Lista de animais que estão em um lar temporário
+        'Animal => Info',
+        'Colaborador'
+    ],
+
+    Doações = [ // Lista
+        'Usuário(contato e número)',
+        'Descrição do item',
+        'Tipo de item' // Ração, dinheiro, remédio...
+    ],
+
+    Publicações = [// Lista
+        'Info',
+        'Imagens'
+    ],
+
+    Denuncias = [ // Lista
+        'Info'
+    ],
+
+    Colaboradores = [ // Lista
+        'Info'
+    ]
+]
